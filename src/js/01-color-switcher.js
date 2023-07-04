cb608b5f239a96d153b7f9d4a5f9dff5192b1207
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2023-07-04 23:13:18 +0200

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+
+startBtn.addEventListener('click', startColor);
+stopBtn.addEventListener('click', stopColor);
+
+function startColor() {
+    timerId = setInterval(getBgColor, 1000);
+
+    startBtn.toggleAttribute('disabled');
+}
+
+function stopColor() {
+    clearInterval(timerId);
+
+    startBtn.removeAttribute('disabled');
+}
+
+function getBgColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
 
